4f747c76014e5e4a40aee583ca6de9e12846ca2b
johnSickels <46609057+johnsickels@users.noreply.github.com>
2019-04-09 22:56:15 -0400

    polish style

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -14,7 +14,7 @@ $("#add-train").on("click", function (event) {
     event.preventDefault();
     var trainName = $("#train-name-input").val().trim();
     var destination = $("#destination-input").val().trim();
-    var firstTrainTime = moment($("#first-train-time-input").val().trim(), 'HH:mm').format('X');
+    var firstTrainTime = $("#first-train-time-input").val().trim();
     var frequency = $("#frequency-input").val().trim();
     var newTrain = {
         name: trainName,
@@ -36,23 +36,31 @@ database.ref().on("child_added", function (childSnapshot) {
     var firstTrainTime = childSnapshot.val().first;
     var frequency = childSnapshot.val().frequency;
 
-    var firstTrain = moment(firstTrainTime, "HH:mm");
+    var firstTrain = moment(firstTrainTime, "HH:mm").subtract(1, 'years');
     console.log("first train: " + firstTrain);
-    var timeRemainder = moment().diff(moment.unix(firstTrain), "minutes") % frequency;
+    var timeRemainder = moment().diff(moment(firstTrain), "minutes") % frequency;
     console.log('time remainder: ' + timeRemainder);
     var minutesAway = frequency - timeRemainder;
     console.log('minutesAway: ' + minutesAway);
-    var nextArrival = moment().add(minutesAway, "m").format("HH:mm A");
+    var nextArrival = moment().add(minutesAway, "m").format('HH:mm');
     console.log('next arrival: ' + nextArrival);
+    var key = childSnapshot.key;
 
     var newRow = $("<tr>").append(
         $("<td>").text(trainName),
         $("<td>").text(destination),
         $("<td>").text(frequency),
         $("<td>").text(nextArrival),
-        $("<td>").text(minutesAway)
+        $("<td>").text(minutesAway),
+        $("<td>").html("<button class='arrival btn btn-danger btn-xs' data-key='" + key + "'>X</button>")
 
     );
+
+    $(document).on("click", ".arrival", function () {
+        keyref = $(this).attr("data-key");
+        database.ref().child(keyref).remove();
+        window.location.reload();
+    });
 
     $("#train-table > tbody").append(newRow);
 });
